3ea0c87fb42dcd78f435d2bce293106a18cf878f
AlmuHusan <38337359+AlmuHusan@users.noreply.github.com>
2021-04-19 18:48:14 -0400

    Base Image Scripts

---
+++ ECG_Image_Scripts/ImageLabeler.py
@@ -0,0 +1,31 @@
+from os import listdir
+from os.path import isfile, join
+import pandas as pd
+path = r"G:\SD 2020\pythonImage 2\data"
+
+# call listdir() method
+# path is a directory of which you want to list
+directories = listdir(path)
+fileName=[]
+pottasium=[]
+# This would print all the files and directories
+for file in directories:
+    fileName.append(file)
+    pottasium.append(file[:3])
+for i in range(len(pottasium)):
+    pottasium[i]=int(float(pottasium[i])*10)
+    if(pottasium[i]<=30):
+        pottasium[i]=0
+    elif(pottasium[i]>=31 and pottasium[i]<=34):
+        pottasium[i]=1
+    elif(pottasium[i]>=35 and pottasium[i]<=55):
+        pottasium[i]=2
+    elif(pottasium[i]>=56 and pottasium[i]<=70):
+        pottasium[i]=3
+    else:
+        pottasium[i]=4
+d = {'fileName': fileName, 'pottasium': pottasium}
+
+df = pd.DataFrame(data=d)
+print(df)
+df.to_csv("imageLabels.csv",header=False, index=False)
---
+++ ECG_Image_Scripts/imageGenerator.py
@@ -0,0 +1,93 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+from skimage import img_as_float
+from skimage import io, color, morphology
+from skimage.transform import rotate
+from PIL import Image
+import matplotlib.pyplot as plt
+import numpy as np
+import random
+import decimal
+
+supervised=True
+pottasiumList=[0]
+randPot = [format(1 + (x * .1),'1.1f') for x in range(0, 81)]
+print(randPot)
+for i in randPot:
+    pottasiumList=[0]
+    for j in range(5):
+        #Generate random pattasium level from 1.0 to 9.0
+        #randPot=float(decimal.Decimal(random.randrange(10, 90))/10)
+        #print(randPot)
+        if supervised:
+            pottasiumList.append(i)
+        image = img_as_float(color.rgb2gray(io.imread('images/'+i+".png")))
+
+        image=np.fliplr(image)
+        image=rotate(image,180)
+        image_binary = image < 0.5
+        out_skeletonize = morphology.skeletonize(image_binary)
+        out_thin = morphology.thin(image_binary)
+        arr=out_thin.astype(np.uint8)
+        arr=np.matrix.transpose(arr)
+
+        #np.savetxt("arr.txt", arr ,delimiter=',', fmt='%d')
+        result = np.where(arr == 1)
+
+        #print('Tuple of arrays returned : ', result)
+        #print(np.flipud(result[0]))
+
+        #plot without noise
+        #plt.plot(result[0],result[1])
+        #plt.show()
+
+
+
+        slicedX=result[0][::2]
+        slicedY=result[1][::2]
+        mu, sigma = 0, 3
+        # creating a noise with the same dimension as the dataset (2,2)
+        noise = np.random.normal(mu, sigma, [1,slicedY.size])
+        #Remove graph UI
+        plt.xticks([])
+        plt.yticks([])
+        plt.axis('off')
+        plt.title('')
+        #plot graph +noise
+
+        plt.plot(slicedX,slicedY+noise[0])
+        plt.figure()
+    if supervised:
+        for i in plt.get_fignums():
+            fig =plt.figure(i)
+            potLevel=pottasiumList[i-1]
+            print(potLevel)
+            plt.savefig("data/"+'{}'.format(potLevel)+"%d.png" % i)
+    else:
+
+        for i in plt.get_fignums():
+            fig = plt.figure(i)
+            plt.savefig('data/figure%d.png' % i)
+    plt.figure().clear()
+    plt.close('all')
+
+
+#How the image is process chart
+"""
+f, (ax0, ax1, ax2) = plt.subplots(1, 3, figsize=(10, 3))
+ax0.imshow(image, cmap='gray')
+ax0.set_title('Input')
+
+ax1.imshow(out_skeletonize, cmap='gray')
+ax1.set_title('Skeletonize')
+
+ax2.imshow(out_thin, cmap='gray')
+ax2.set_title('Thin')
+
+plt.savefig('char_out.png')
+plt.show()
+"""
